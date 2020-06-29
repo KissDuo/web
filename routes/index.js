64dd8e406199fa9a716860ce94011da581b7c0df
KissDuo <303135526@qq.com>
2020-06-29 15:36:08 +0800

    新增app内容

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,29 @@ var express = require('express');
 var router = express.Router();
 var Util = require('../public/javascripts/util.js');
 
-/* 首页 */
+/* 首页(小程序) */
 router.get('/', function(req, res, next) {
   if(Util.checkAgentIsMobile(req)){
-    res.render('mobile/index');
+    res.render('mobile/index',{
+      route : "index"
+    });
   }else{
-    res.render('pc/index');
+    res.render('pc/index',{
+      route : "index"
+    });
+  }
+});
+
+/* app */
+router.get('/app', function(req, res, next) {
+  if(Util.checkAgentIsMobile(req)){
+    res.render('mobile/app',{
+      route : "app"
+    });
+  }else{
+    res.render('pc/app',{
+      route : "app"
+    });
   }
 });
 
